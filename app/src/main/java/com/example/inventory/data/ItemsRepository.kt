71556040f090cb79fa8c909216ebd613b60b9136
valery2698 <valery2698@hotmail.com>
2023-11-26 20:06:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/inventory/data/ItemsRepository.kt
@@ -0,0 +1,19 @@
+package com.example.inventory.data
+
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository that provides insert, update, delete, and retrieve of [Item] from a given data source.
+ */
+interface ItemsRepository {
+
+    fun getAllItemsStream(): Flow<List<Item>>
+
+    fun getItemStream(id: Int): Flow<Item?>
+
+    suspend fun insertItem(item: Item)
+
+    suspend fun deleteItem(item: Item)
+
+    suspend fun updateItem(item: Item)
+}
---
+++ app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -0,0 +1,44 @@
+package com.example.inventory.ui
+
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.inventory.InventoryApplication
+import com.example.inventory.ui.home.HomeViewModel
+import com.example.inventory.ui.item.ItemDetailsViewModel
+import com.example.inventory.ui.item.ItemEditViewModel
+import com.example.inventory.ui.item.ItemEntryViewModel
+
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        // Initializer for ItemEditViewModel
+        initializer {
+            ItemEditViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+        // Initializer for ItemEntryViewModel
+        initializer {
+            ItemEntryViewModel(inventoryApplication().container.itemsRepository)
+        }
+
+        // Initializer for ItemDetailsViewModel
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle()
+            )
+        }
+
+        // Initializer for HomeViewModel
+        initializer {
+            HomeViewModel(inventoryApplication().container.itemsRepository)
+        }
+    }
+}
+
+
+fun CreationExtras.inventoryApplication(): InventoryApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as InventoryApplication)
